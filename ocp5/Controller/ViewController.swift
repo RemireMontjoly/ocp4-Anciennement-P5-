434009c7f45152903135be874ce992ac7284ec01
Dino <dino.pith@gmail.com>
2019-01-20 11:00:39 +0100

    Adding button checked property

--- ocp5/Controller/ViewController.swift
+++ ocp5/Controller/ViewController.swift
@@ -13,31 +13,38 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var button1: UIButton!
     
+    @IBOutlet weak var button2: UIButton!
+    
+    @IBOutlet weak var button3: UIButton!
+    
+    
     
     @IBAction func button1Pressed(_ sender: UIButton) {
-        if let image = UIImage(named: "Selected") {
+        if let image = UIImage(named:"Selected") {
             sender.setImage(image, for: .normal)
+            button2.imageView?.isHidden = true
+            button3.imageView?.isHidden = true
         }
         
     }
     
-  
     
     @IBAction func button2Pressed(_ sender: UIButton) {
-        if let image = UIImage(named: "Selected") {
+        if let image = UIImage(named:"Selected") {
             sender.setImage(image, for: .normal)
+            button1.imageView?.isHidden = true
+            button3.imageView?.isHidden = true
         }
     }
-    
     
     
     @IBAction func button3Pressed(_ sender: UIButton) {
-        if let image = UIImage(named: "Selected") {
+        if let image = UIImage(named:"Selected") {
             sender.setImage(image, for: .normal)
+            button1.imageView?.isHidden = true
+            button2.imageView?.isHidden = true
         }
     }
-    
-    
     
     
     override func viewDidLoad() {
