e640a7da46b4781f7d7fbb46daed236a84f08613
Dino <dino.pith@gmail.com>
2019-02-02 09:26:33 +0100

    Adding buttonTapped fonctionality to central buttons

--- ocp5/View/GridView.swift
+++ ocp5/View/GridView.swift
@@ -2,30 +2,20 @@
 //  GridView.swift
 //  ocp5
 //
-//  Created by pith on 26/01/2019.
+//  Created by pith on 28/01/2019.
 //  Copyright © 2019 dino. All rights reserved.
 //
 
 import UIKit
-/*
-class GridView: UIView {
-let view = ViewController()
-    
-    enum SelectedGrid {
-        case rectangleUp, rectangleDown, fourSquare
-    }
 
-    
-    var selectedGrid: SelectedGrid = .rectangleUp
-    
-    
-    func setGrid (grid: SelectedGrid) {
-        switch selectedGrid {
-        case .rectangleUp: break
-        case .rectangleDown: break
-        case .fourSquare: break
-        }
+class GridView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
     }
+    */
 
 }
- */
